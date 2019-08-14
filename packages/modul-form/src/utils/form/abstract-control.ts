95a95c4a03bc0a4ef3d73639d930fd2d62d1bad6
Charles Maheu <choc.mah@gmail.com>
2019-08-14 17:08:45 -0400

    test3

--- packages/modul-form/src/utils/form/abstract-control.ts
+++ packages/modul-form/src/utils/form/abstract-control.ts
@@ -14,7 +14,7 @@ import { ControlValidator } from './validators/control-validator';
 export abstract class AbstractControl {
     public htmlElement: HTMLElement | undefined;
 
-    // a left change
+    // a left change 2
     protected readonly _validationGuard: ControlValidationGuard = DefaultValidationGuard;
     protected _parent: FormGroup | FormArray;
     protected _editionContext: ControlEditionContext = ControlEditionContext.None;
