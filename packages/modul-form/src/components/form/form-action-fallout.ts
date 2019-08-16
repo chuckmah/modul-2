257a3d532851dcf87270633070914208ae8b0b25
Charles <choc.mah@gmail.com>
2019-08-15 22:25:54 -0400

    breaking change into form #5

--- packages/modul-form/src/components/form/form-action-fallout.ts
+++ packages/modul-form/src/components/form/form-action-fallout.ts
@@ -3,6 +3,6 @@ import { FormActions } from './form-action-type';
 
 export type FormActionFallout = {
     action: FormActions,
-    // im a breaking change 4
+    // im a breaking change 5
     fallout: (form: MForm) => void;
 };
